9b9feb74103816908c1044685fba5db466f40691
yangchenhui <t783072156@outlook.com>
2021-03-01 20:01:46 +0800

    :lipstick: 更新部分样式和内容

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -21,8 +21,8 @@ export default {
   skillItem1: 'JavaScript和React(熟练)',
   skillItem3: 'HTML和CSS(熟练)',
   skillItem4: 'TypeScript(熟悉)',
-  skillItem5: 'antd、element(熟悉)',
-  skillItem6: 'TCP/IP(了解)',
+  skillItem5: 'antd、element-ui(熟悉)',
+  skillItem6: 'TCP/IP网络(了解)',
   skillItem7: 'Node.js和Webpack(了解)',
   skillItem8: '常见的的算法和数据结构(了解)',
   skillItem9: 'Graphql和Dgraph(了解)',
@@ -42,23 +42,23 @@ export default {
   bytedanceProfile:
     '主要负责日常业务的迭代需求，日常业务包括ToB方面厂商系统、运管系统和内部的数据平台，ToC方面懂车帝app和小程序。',
   bytedanceContent1:
-    '完成基于Umi的React项目的日常开发需求，包括代码实现、接口联调、自测、提测、代码CR、小流量验证和正式上线；',
+    '完成基于Umi的React项目的日常开发需求，包括代码实现、接口联调、自测、提测、代码CR、小流量验证和正式上线工作；',
   bytedanceContent2:
     '基于ECharts可视化库，解决原有代码重复、不可复用的问题，完成了内部数据平台项目公用组件的封装工作；',
   bytedanceContent3: '参与需求评审，周会进行技术分享；',
   valueSystemName: '科创企业智能估值系统(学院项目)',
   valueSystemTime: '2021年1月 - 2021年3月',
   valueSystemProfile:
-    '主要负责前端开发方面的任务分配、需求讲解以及技术指导方面工作。',
+    '主要负责前端方面的任务分配、需求讲解以及技术指导等工作。',
   valueSystemContent1:
     '完成产业链族谱需求，采用AntV的X6图编辑引擎，解决了产业链关系图的展示问题；',
   valueSystemContent2:
     '利用EOLINKER工具管理项目接口，利用云效安排迭代、需求以及跟踪进度；',
-  personProjectName: ' web在线音乐(个人项目)',
+  personProjectName: ' 在线音乐(个人项目)',
   personProjectTime: '2020年2月 - 2020年3月',
   personProjectProfile:
-    '主要工作是页面的设计排版、交互逻辑以及页面跳转逻辑的实现，并进行了代码和性能的优化。',
+    '完成页面的设计排版，实现具体的交互逻辑，包括搜索、播放、私人FM等功能。',
   personProjectContent1:
-    '基于网易云API，实现web在线搜索、播放音乐、查看MV、私人FM、历史播放、主页等功能;',
+    '后端基于网易云API开源库实现，前端使用Vue框架开发，利用Vue及其相关生态解决了路由跳转、嵌套以及全局状态管理等实际问题；',
   personProjectContent2: '项目地址',
 };
